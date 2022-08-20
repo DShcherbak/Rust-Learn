2b8984d44a11c06bc7a604a6cc3a71a8ce8302f0
Denys Shcherbak <denys.shcherbak.official@gmail.com>
2022-08-20 12:28:37 +0300

    removed target from chapter-3

--- rust-book/chapter-3/target/debug/deps/chapter_3-74a74ec7ae7729bb.d
+++
@@ -1,5 +0,0 @@
-c:\Users\denys\OneDrive\Documents\Rust\Rust-Learn\rust-book\chapter-3\target\debug\deps\chapter_3-74a74ec7ae7729bb.rmeta: src\main.rs
-
-c:\Users\denys\OneDrive\Documents\Rust\Rust-Learn\rust-book\chapter-3\target\debug\deps\chapter_3-74a74ec7ae7729bb.d: src\main.rs
-
-src\main.rs:
